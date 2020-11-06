e0a01833a828f7e50e83e99368db0e092591ba87
K <JK063102@cerner.net>
2020-11-06 10:29:04 +0530

    commiting Factorypattern

---
+++ FactoryPattern/CreditCardFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class CreditCardFactory
+    {
+        public static IcreditCard GetCreditCard(string cardType)
+        {
+            IcreditCard cardDetails = null;
+            if (cardType == "MoneyBack")
+            {
+                cardDetails = new MoneyBack();
+            }
+            else if (cardType == "Titanium")
+            {
+                cardDetails = new Titanium();
+            }
+            else if (cardType == "Platinum")
+            {
+                cardDetails = new Platinum();
+            }
+            return cardDetails;
+        }
+    }
+}
